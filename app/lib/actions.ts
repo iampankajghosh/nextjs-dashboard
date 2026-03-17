3f103de39e06b1b5252f562f61a0b8acafcab0f3
Pankaj Ghosh <dev.pankajghosh@gmail.com>
2026-03-18 00:00:36 +0530

    feat: implement authentication flow with login page and user session management

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -104,4 +106,28 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    const redirectTo = formData.get("redirectTo") as string;
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirectTo,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong";
+      }
+    }
+    throw error;
+  }
 }
